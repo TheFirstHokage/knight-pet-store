b879bae87e4693b2071351dcc9b640d78644fbb7
TheFirstHokage <kendrickutwhite@gmail.com>
2022-06-07 23:35:04 -0700

    initial commit

---
+++ src/main/java/knight/springframework/petstoreapp/PetStoreAppApplication.java
@@ -0,0 +1,13 @@
+package knight.springframework.petstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/knight/springframework/petstoreapp/PetStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package knight.springframework.petstoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
